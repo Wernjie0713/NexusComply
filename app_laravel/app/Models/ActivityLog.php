c5bc611a7e94a6a01485f0fe693825e817c0b26e
Tang Yan Qing <tangqing@graduate.utm.my>
2025-06-14 15:57:06 +0800

    Create ActivityLog.php

---
+++ app_laravel/app/Models/ActivityLog.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ActivityLog extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'action_type',
+        'target_type',
+        'details',
+        'user_id',
+        'created_at'
+    ];
+
+    public $timestamps = false; // Disable timestamps since we only have created_at
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+} 
